3a1f09e805f02f6072c8e537779bbcb982d3534e
Geremia <ageremia@labs.neit.edu>
2018-03-28 10:23:53 -0400

    generate a raqndom number

---
+++ mult.php
@@ -0,0 +1,13 @@
+<?php
+
+$size = mt_rand(5,10);
+$table = "<table>\n";
+
+for($rows = 1; $rows <= $size; $rows++)
+{
+	$table .= "\t<tr>";
+	$table .= "\t</tr>\n";
+}
+$table .= "</table>\n";
+
+?>
