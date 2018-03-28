a9cb017a755201f3e4a12095f255b31a011783ef
Geremia <ageremia@labs.neit.edu>
2018-03-28 10:35:32 -0400

    Finish table

--- mult.php
+++ mult.php
@@ -6,8 +6,22 @@ $table = "<table>\n";
 for($rows = 1; $rows <= $size; $rows++)
 {
 	$table .= "\t<tr>";
+	for ($cols = 1; $cols <= $size; $cols++):
+		$table .= "<td>" . $rows*$cols . "</td>";
+	endfor;
 	$table .= "\t</tr>\n";
 }
 $table .= "</table>\n";
 
 ?>
+
+<!DOCTYPE html>
+<html lang="em">
+<head>
+	<meta charset="UTF-8">
+	<title>Multiplication Table</title>
+</head>
+<body>
+	<?php echo $table; ?>
+</body>
+</html>
